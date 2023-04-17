b130ecad9f23501b59c984b459780e3863fd266a
Szemin <116577945+Szemin@users.noreply.github.com>
2023-04-17 14:12:51 +0200

    Working console app

--- EmailSender.Client/Program.cs
+++ EmailSender.Client/Program.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using EmailSender;
+using System;
 namespace EmailClient;
 
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
+        var email = new Email(new EmailParams
+        {
+            HostSmtp = "smtp.gmail.com",
+            Port = 587,
+            EnableSsl= true,
+            SenderName = "WŁADCA GMAILA",
+            SenderEmail = "szemintest@gmail.com",
+            SenderEmailPassword = "ewccmfmueiqqxtbu"
+        });
+        Console.WriteLine("wysyłanie maila ...");
 
+        for (int i = 1; i <= 5; i++)
+        {
+            await email.Send(
+                "Email testowy z aplikacji",
+                "Przykładowy mail",
+                "szemintest@gmail.com");
+
+            Console.WriteLine("mail wysłano");
+        }
     }
 }
